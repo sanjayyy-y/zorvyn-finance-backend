0e7de1baf059dda103b8d6df90ea3868e09262dd
sanjayyy-y <sanjaypradeep111@gmail.com>
2026-04-02 12:48:24 +0530

    feat: vercel cofiguration

--- server.js
+++ server.js
@@ -12,9 +12,13 @@ connectDB();
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode.`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  const server = app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} in ${process.env.NODE_ENV} mode.`);
+  });
+}
+
+module.exports = app;
 
 process.on('unhandledRejection', err => {
   console.error('UNHANDLED REJECTION! Shutting down...');
